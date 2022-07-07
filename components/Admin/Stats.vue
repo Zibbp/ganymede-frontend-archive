1931faa34f4448407064b77947291cd37e789da6
Zibbp <developerzippy@gmail.com>
2022-07-07 17:09:30 -0500

    Groundwork for admin pages

---
+++ components/Admin/Stats.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="surface-ground px-4 py-5 md:px-6 lg:px-8">
+    <div class="grid w-justify-center w-items-center">
+      <div class="col-12 md:col-6 lg:col-3">
+        <div class="surface-card shadow-2 p-3 border-round">
+          <div class="flex justify-content-between mb-3">
+            <div>
+              <span class="block text-500 font-medium mb-3">VODs</span>
+              <div class="text-900 font-medium text-xl">
+                {{ stats.vod_count.toLocaleString() }}
+              </div>
+            </div>
+            <div
+              class="flex align-items-center justify-content-center bg-blue-100 border-round"
+              style="width: 2.5rem; height: 2.5rem"
+            >
+              <i class="pi pi-video text-blue-500 text-xl"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-12 md:col-6 lg:col-3">
+        <div class="surface-card shadow-2 p-3 border-round">
+          <div class="flex justify-content-between mb-3">
+            <div>
+              <span class="block text-500 font-medium mb-3">Channels</span>
+              <div class="text-900 font-medium text-xl">
+                {{ stats.channel_count.toLocaleString() }}
+              </div>
+            </div>
+            <div
+              class="flex align-items-center justify-content-center bg-orange-100 border-round"
+              style="width: 2.5rem; height: 2.5rem"
+            >
+              <i class="pi pi-user text-orange-500 text-xl"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { useApi } from "~/composables/useApi";
+
+const { data: stats } = await useAsyncData(`admin-stats`, () =>
+  useApi(`/api/v1/admin/stats`, {
+    method: "GET",
+    credentials: "include",
+  })
+);
+</script>
+
+<style lang="scss" scoped></style>
---
+++ components/Admin/VodsTable.vue
@@ -0,0 +1,564 @@
+<template>
+  <div>
+    <div v-if="pending">Loading...</div>
+    <div v-else>
+      <div>
+        <div class="card">
+          <Toolbar class="mb-4">
+            <template #start>
+              <Button
+                label="New"
+                icon="pi pi-plus"
+                class="p-button-success mr-2"
+                @click="openNew"
+              />
+              <Button
+                label="Delete"
+                icon="pi pi-trash"
+                class="p-button-danger"
+                @click="confirmDeleteSelected"
+                :disabled="!selectedProducts || !selectedProducts.length"
+              />
+            </template>
+
+            <template #end>
+              <FileUpload
+                mode="basic"
+                accept="image/*"
+                :maxFileSize="1000000"
+                label="Import"
+                chooseLabel="Import"
+                class="mr-2 inline-block"
+              />
+              <Button
+                label="Export"
+                icon="pi pi-upload"
+                class="p-button-help"
+                @click="exportCSV($event)"
+              />
+            </template>
+          </Toolbar>
+
+          <DataTable
+            ref="dt"
+            :value="vods"
+            v-model:selection="selectedProducts"
+            dataKey="id"
+            :paginator="true"
+            :rows="10"
+            :filters="filters"
+            paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
+            :rowsPerPageOptions="[5, 10, 25]"
+            currentPageReportTemplate="Showing {first} to {last} of {totalRecords} products"
+            responsiveLayout="scroll"
+            rowHover="true"
+          >
+            <template #header>
+              <div
+                class="table-header flex flex-column md:flex-row md:justiify-content-between"
+              >
+                <h5 class="mb-2 md:m-0 p-as-md-center">Manage Products</h5>
+                <span class="p-input-icon-left">
+                  <i class="pi pi-search" />
+                  <InputText
+                    v-model="filters['global'].value"
+                    placeholder="Search..."
+                  />
+                </span>
+              </div>
+            </template>
+
+            <Column
+              selectionMode="multiple"
+              style="width: 3rem"
+              :exportable="false"
+            ></Column>
+            <Column
+              field="id"
+              header="ID"
+              :sortable="true"
+              style="max-width: 5rem"
+            >
+              <template #body="slotProps">
+                <span :title="slotProps.data.id" class="w-line-clamp-1">{{
+                  slotProps.data.id
+                }}</span>
+              </template>
+            </Column>
+            <Column field="ext_id" header="Ext ID" :sortable="true"></Column>
+            <Column
+              header="Channel"
+              sortable
+              filterField="edges.channel.display_name"
+              sortField="edges.channel.display_name"
+            >
+              <template #body="{ data }">
+                <img
+                  :src="config.cdnURL + data.edges.channel.image_path"
+                  width="30"
+                  style="vertical-align: middle"
+                  class="w-inline border-circle"
+                />
+                <span class="image-text">{{
+                  data.edges.channel.display_name
+                }}</span>
+              </template>
+            </Column>
+            <Column
+              field="title"
+              header="Title"
+              :sortable="true"
+              style="max-width: 25rem"
+            >
+              <template #body="slotProps">
+                <span :title="slotProps.data.title" class="w-line-clamp-1">{{
+                  slotProps.data.title
+                }}</span>
+              </template>
+            </Column>
+
+            <Column field="type" header="Type" :sortable="true">
+              <template #body="slotProps">
+                <span
+                  :class="
+                    'vod-type-badge type-' +
+                    (slotProps.data.type
+                      ? slotProps.data.type.toLowerCase()
+                      : '')
+                  "
+                  >{{ slotProps.data.type.toUpperCase() }}</span
+                >
+              </template>
+            </Column>
+            <Column field="created_at" header="Created At" :sortable="true">
+              <template #body="slotProps">
+                <span>{{
+                  dayjs(slotProps.data.created_at).format("YYYY/MM/DD")
+                }}</span>
+              </template>
+            </Column>
+            <Column :exportable="false" style="min-width: 8rem">
+              <template #body="slotProps">
+                <Button
+                  icon="pi pi-pencil"
+                  class="p-button-rounded p-button-success mr-2"
+                  @click="editProduct(slotProps.data)"
+                />
+                <Button
+                  icon="pi pi-trash"
+                  class="p-button-rounded p-button-warning"
+                  @click="confirmDeleteProduct(slotProps.data)"
+                />
+              </template>
+            </Column>
+          </DataTable>
+        </div>
+
+        <Dialog
+          v-model:visible="productDialog"
+          :style="{ width: '450px' }"
+          header="Product Details"
+          :modal="true"
+          class="p-fluid"
+        >
+          <img
+            src="https://www.primefaces.org/wp-content/uploads/2020/05/placeholder.png"
+            :alt="product.image"
+            class="product-image"
+            v-if="product.image"
+          />
+          <div class="field">
+            <label for="name">Name</label>
+            <InputText
+              id="name"
+              v-model.trim="product.name"
+              required="true"
+              autofocus
+              :class="{ 'p-invalid': submitted && !product.name }"
+            />
+            <small class="p-error" v-if="submitted && !product.name"
+              >Name is required.</small
+            >
+          </div>
+          <div class="field">
+            <label for="description">Description</label>
+            <Textarea
+              id="description"
+              v-model="product.description"
+              required="true"
+              rows="3"
+              cols="20"
+            />
+          </div>
+
+          <div class="field">
+            <label for="inventoryStatus" class="mb-3">Inventory Status</label>
+            <Dropdown
+              id="inventoryStatus"
+              v-model="product.inventoryStatus"
+              :options="statuses"
+              optionLabel="label"
+              placeholder="Select a Status"
+            >
+              <template #value="slotProps">
+                <div v-if="slotProps.value && slotProps.value.value">
+                  <span
+                    :class="'product-badge status-' + slotProps.value.value"
+                    >{{ slotProps.value.label }}</span
+                  >
+                </div>
+                <div v-else-if="slotProps.value && !slotProps.value.value">
+                  <span
+                    :class="
+                      'product-badge status-' + slotProps.value.toLowerCase()
+                    "
+                    >{{ slotProps.value }}</span
+                  >
+                </div>
+                <span v-else>
+                  {{ slotProps.placeholder }}
+                </span>
+              </template>
+            </Dropdown>
+          </div>
+
+          <div class="field">
+            <label class="mb-3">Category</label>
+            <div class="formgrid grid">
+              <div class="field-radiobutton col-6">
+                <RadioButton
+                  id="category1"
+                  name="category"
+                  value="Accessories"
+                  v-model="product.category"
+                />
+                <label for="category1">Accessories</label>
+              </div>
+              <div class="field-radiobutton col-6">
+                <RadioButton
+                  id="category2"
+                  name="category"
+                  value="Clothing"
+                  v-model="product.category"
+                />
+                <label for="category2">Clothing</label>
+              </div>
+              <div class="field-radiobutton col-6">
+                <RadioButton
+                  id="category3"
+                  name="category"
+                  value="Electronics"
+                  v-model="product.category"
+                />
+                <label for="category3">Electronics</label>
+              </div>
+              <div class="field-radiobutton col-6">
+                <RadioButton
+                  id="category4"
+                  name="category"
+                  value="Fitness"
+                  v-model="product.category"
+                />
+                <label for="category4">Fitness</label>
+              </div>
+            </div>
+          </div>
+
+          <div class="formgrid grid">
+            <div class="field col">
+              <label for="price">Price</label>
+              <InputNumber
+                id="price"
+                v-model="product.price"
+                mode="currency"
+                currency="USD"
+                locale="en-US"
+              />
+            </div>
+            <div class="field col">
+              <label for="quantity">Quantity</label>
+              <InputNumber
+                id="quantity"
+                v-model="product.quantity"
+                integeronly
+              />
+            </div>
+          </div>
+          <template #footer>
+            <Button
+              label="Cancel"
+              icon="pi pi-times"
+              class="p-button-text"
+              @click="hideDialog"
+            />
+            <Button
+              label="Save"
+              icon="pi pi-check"
+              class="p-button-text"
+              @click="saveProduct"
+            />
+          </template>
+        </Dialog>
+
+        <Dialog
+          v-model:visible="deleteProductDialog"
+          :style="{ width: '450px' }"
+          header="Confirm"
+          :modal="true"
+        >
+          <div class="confirmation-content">
+            <i
+              class="pi pi-exclamation-triangle mr-3"
+              style="font-size: 2rem"
+            />
+            <span v-if="product"
+              >Are you sure you want to delete <b>{{ product.name }}</b
+              >?</span
+            >
+          </div>
+          <template #footer>
+            <Button
+              label="No"
+              icon="pi pi-times"
+              class="p-button-text"
+              @click="deleteProductDialog = false"
+            />
+            <Button
+              label="Yes"
+              icon="pi pi-check"
+              class="p-button-text"
+              @click="deleteProduct"
+            />
+          </template>
+        </Dialog>
+
+        <Dialog
+          v-model:visible="deleteProductsDialog"
+          :style="{ width: '450px' }"
+          header="Confirm"
+          :modal="true"
+        >
+          <div class="confirmation-content">
+            <i
+              class="pi pi-exclamation-triangle mr-3"
+              style="font-size: 2rem"
+            />
+            <span v-if="product"
+              >Are you sure you want to delete the selected products?</span
+            >
+          </div>
+          <template #footer>
+            <Button
+              label="No"
+              icon="pi pi-times"
+              class="p-button-text"
+              @click="deleteProductsDialog = false"
+            />
+            <Button
+              label="Yes"
+              icon="pi pi-check"
+              class="p-button-text"
+              @click="deleteSelectedProducts"
+            />
+          </template>
+        </Dialog>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import DataTable from "primevue/datatable";
+import Column from "primevue/column";
+import ColumnGroup from "primevue/columngroup"; //optional for column grouping
+import Row from "primevue/row"; //optional for row
+import { ref, onMounted } from "vue";
+import { FilterMatchMode } from "primevue/api";
+import { useToast } from "primevue/usetoast";
+import { useApi } from "~/composables/useApi";
+import dayjs from "dayjs/esm";
+const config = useRuntimeConfig().public;
+
+const { pending, data: vods } = useLazyAsyncData("admin-vods", () =>
+  useApi(`/api/v1/vod`, {
+    method: "GET",
+    credentials: "include",
+  })
+);
+
+const toast = useToast();
+const dt = ref();
+const products = ref();
+const productDialog = ref(false);
+const deleteProductDialog = ref(false);
+const deleteProductsDialog = ref(false);
+const product = ref({});
+const selectedProducts = ref();
+const filters = ref({
+  global: { value: null, matchMode: FilterMatchMode.CONTAINS },
+});
+const submitted = ref(false);
+const statuses = ref([
+  { label: "archive", value: "ARCHIVE" },
+  { label: "LOWSTOCK", value: "lowstock" },
+  { label: "OUTOFSTOCK", value: "outofstock" },
+]);
+
+const formatCurrency = (value) => {
+  if (value)
+    return value.toLocaleString("en-US", {
+      style: "currency",
+      currency: "USD",
+    });
+  return;
+};
+const openNew = () => {
+  product.value = {};
+  submitted.value = false;
+  productDialog.value = true;
+};
+const hideDialog = () => {
+  productDialog.value = false;
+  submitted.value = false;
+};
+const saveProduct = () => {
+  submitted.value = true;
+
+  if (product.value.name.trim()) {
+    if (product.value.id) {
+      product.value.inventoryStatus = product.value.inventoryStatus.value
+        ? product.value.inventoryStatus.value
+        : product.value.inventoryStatus;
+      products.value[findIndexById(product.value.id)] = product.value;
+      toast.add({
+        severity: "success",
+        summary: "Successful",
+        detail: "Product Updated",
+        life: 3000,
+      });
+    } else {
+      product.value.id = createId();
+      product.value.code = createId();
+      product.value.image = "product-placeholder.svg";
+      product.value.inventoryStatus = product.value.inventoryStatus
+        ? product.value.inventoryStatus.value
+        : "INSTOCK";
+      products.value.push(product.value);
+      toast.add({
+        severity: "success",
+        summary: "Successful",
+        detail: "Product Created",
+        life: 3000,
+      });
+    }
+
+    productDialog.value = false;
+    product.value = {};
+  }
+};
+const editProduct = (prod) => {
+  product.value = { ...prod };
+  productDialog.value = true;
+};
+const confirmDeleteProduct = (prod) => {
+  product.value = prod;
+  deleteProductDialog.value = true;
+};
+const deleteProduct = () => {
+  products.value = products.value.filter((val) => val.id !== product.value.id);
+  deleteProductDialog.value = false;
+  product.value = {};
+  toast.add({
+    severity: "success",
+    summary: "Successful",
+    detail: "Product Deleted",
+    life: 3000,
+  });
+};
+const findIndexById = (id) => {
+  let index = -1;
+  for (let i = 0; i < products.value.length; i++) {
+    if (products.value[i].id === id) {
+      index = i;
+      break;
+    }
+  }
+
+  return index;
+};
+const createId = () => {
+  let id = "";
+  var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (var i = 0; i < 5; i++) {
+    id += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return id;
+};
+const exportCSV = () => {
+  dt.value.exportCSV();
+};
+const confirmDeleteSelected = () => {
+  deleteProductsDialog.value = true;
+};
+const deleteSelectedProducts = () => {
+  products.value = products.value.filter(
+    (val) => !selectedProducts.value.includes(val)
+  );
+  deleteProductsDialog.value = false;
+  selectedProducts.value = null;
+  toast.add({
+    severity: "success",
+    summary: "Successful",
+    detail: "Products Deleted",
+    life: 3000,
+  });
+};
+</script>
+
+<style lang="scss" scoped>
+.vod-type-badge {
+  border-radius: 2px;
+  padding: 0.25em 0.5rem;
+  text-transform: uppercase;
+  font-weight: 700;
+  font-size: 12px;
+  letter-spacing: 0.3px;
+}
+.type-archive {
+  background-color: #b3e5fc;
+  color: #23547b;
+}
+.table-header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  @media screen and (max-width: 960px) {
+    align-items: start;
+  }
+}
+
+.product-image {
+  width: 50px;
+  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+}
+
+.confirmation-content {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+@media screen and (max-width: 960px) {
+  ::v-deep(.p-toolbar) {
+    flex-wrap: wrap;
+
+    .p-button {
+      margin-bottom: 0.25rem;
+    }
+  }
+}
+.image-text {
+  vertical-align: middle;
+  margin-left: 0.5rem;
+}
+</style>
