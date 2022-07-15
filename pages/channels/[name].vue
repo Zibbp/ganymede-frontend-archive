00956b8c0fa3d3231eacc7a437b113cebd72009d
Zibbp <zibbp@protonmail.com>
2022-07-15 10:09:57 -0500

    Fix: Loading channel vods

--- pages/channels/[name].vue
+++ pages/channels/[name].vue
@@ -2,7 +2,7 @@
   <div>
     <VodHeader :name="channel.display_name" />
     <div class="w-container w-mx-auto">
-      <div v-if="pending">Loading VOds...</div>
+      <div v-if="pending">Loading Vods...</div>
       <div v-else>
         <VodSection :vods="vods"></VodSection>
       </div>
@@ -11,20 +11,50 @@
 </template>
 
 <script setup>
+import { useApi } from "~/composables/useApi";
+import { useToast } from "primevue/usetoast";
+const toast = useToast();
+
 const route = useRoute();
 const config = useRuntimeConfig().public;
 
-const { data: channel } = await useFetch(
-  `${config.apiURL}/api/v1/channel/name/${route.params.name}`
-);
+let channel;
+
+try {
+  channel = await useApi(`/api/v1/channel/name/${route.params.name}`, {
+    method: "GET",
+    credentials: "include",
+  });
+} catch (error) {
+  console.error("Error fetching channel: ", error);
+  toast.add({
+    severity: "error",
+    summary: "Error fetching channel",
+    detail: error.response.data.message,
+    life: 3000,
+  });
+}
 
 const {
   data: vods,
   pending,
   refresh,
-} = await useLazyAsyncData(`vods-${channel.value.id}`, () =>
-  $fetch(`${config.apiURL}/api/v1/vod?channel_id=${channel.value.id}`)
-);
+} = await useLazyAsyncData(`vods-${channel.id}`, () => {
+  try {
+    return useApi(`/api/v1/vod?channel_id=${channel.id}`, {
+      method: "GET",
+      credentials: "include",
+    });
+  } catch (error) {
+    console.error("Error fetching vods: ", error);
+    toast.add({
+      severity: "error",
+      summary: "Error fetching vods",
+      detail: error.response.data.message,
+      life: 3000,
+    });
+  }
+});
 </script>
 
 <style lang="scss" scoped></style>
