03d27e5e3e63d0a0b322dabb7306da245ae52c81
Zibbp <zibbp@protonmail.com>
2022-08-16 09:50:52 -0500

    feat(api): set css content-type

--- server/routes/theme/[theme].js
+++ server/routes/theme/[theme].js
@@ -2,5 +2,6 @@ import fs from 'node:fs'
 import { sendStream } from 'h3'
 
 export default defineEventHandler(event => {
+    setHeader(event, 'Content-Type', 'text/css')
     return sendStream(event, fs.createReadStream(`./node_modules/primevue/resources/themes/${event.context.params.theme}/theme.css`))
 })
